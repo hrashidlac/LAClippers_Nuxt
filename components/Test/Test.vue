3d651df9e8fe9c7d968dee4b9031ce1522ad1c90
Harris Rashid <hrashid@clippers.com>
2022-02-07 14:00:48 -0800

    test

--- components/Test/Test.vue
+++ components/Test/Test.vue
@@ -25,27 +25,24 @@ export default {
     }
   },
   head () {
-    const skip = theJsHasBeenAddedSoSkipIt
-    theJsHasBeenAddedSoSkipIt = true
+  
     return {
       title: 'Dynamics Page',
       script: [
         {
           hid: 'script1',
-          
           once: true,
-          skip,
           src: 'https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.74.1038.0',
+          body: true,
           defer: true,
           // Changed after script load
           callback: () => { this.isScriptLoaded = true } 
         },
         {
-          hid: 'script2',
-          
+          hid: 'script2',  
           once: true,
-          skip,
           src: 'https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.74.1038.0',
+          body: true,
           defer: true,
           // Changed after script load
           callback: () => { this.isScriptLoaded2 = true } 
