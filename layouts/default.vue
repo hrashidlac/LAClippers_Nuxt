c4ccbccb69ad304c01c0b5ea80d449c29db90f93
Harris Rashid <hrashid@clippers.com>
2022-03-03 12:00:46 -0800

    layout file and script loader test

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Layout File!
+    <div>
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    loadLexWebUI () {
+      const lex = document.createElement('script')
+      const lex2 = document.createElement('script')
+      const currentLocation = window.location.origin
+      console.log(currentLocation)
+      lex.src = 'https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.74.1038.0'
+      document.body.appendChild(lex)
+      lex2.src = 'https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.74.1038.0'
+      document.body.appendChild(lex2)
+    }
+  },
+  mounted () {
+    this.loadLexWebUI()
+    console.log('test')
+  }
+}
+</script>
--- pages/test.vue
+++ pages/test.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <Header />
-    <Test />
+    <!-- <Test /> -->
+    <div data-form-block-id="60fc0d57-53ee-eb11-bacb-00224809a9ae"></div>
+    <div id="dfzXoiGg_ZVQ4cQYffm4drGLe7CLsCLEPpE9_BQ0ReuI"></div>
+    <div class="d365-mkt-config" style="display:none" data-website-id="fzXoiGg_ZVQ4cQYffm4drGLe7CLsCLEPpE9_BQ0ReuI" data-hostname="aa8d20baa12c40e281518e9e92b636ad.svc.dynamics.com"></div> 
   </div>
 </template>
+
